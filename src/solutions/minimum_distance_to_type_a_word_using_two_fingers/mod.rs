b910911687b9708e55e1316e879ddc4ac2a0cc94
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-05-09 22:06:54 +0300

    #1320 add KEYBOARD array

--- src/solutions/minimum_distance_to_type_a_word_using_two_fingers/mod.rs
+++ src/solutions/minimum_distance_to_type_a_word_using_two_fingers/mod.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/
 use crate::Solution;
+
+lazy_static! {
+    static ref KEYBOARD: &'static [&'static [char]] = {
+        &[
+            &['A', 'B', 'C', 'D', 'E', 'F'],
+            &['G', 'H', 'I', 'J', 'K', 'L'],
+            &['M', 'N', 'O', 'P', 'Q', 'R'],
+            &['S', 'T', 'U', 'V', 'W', 'X'],
+            &['Y', 'Z'],
+        ]
+    };
+}
+
 impl Solution {
     #[allow(dead_code)]
-    pub fn minimum_distance(word: String) -> i32 {
-        2
-    }
+    pub fn minimum_distance(word: String) -> i32 {}
 }
 
 #[cfg(test)]
