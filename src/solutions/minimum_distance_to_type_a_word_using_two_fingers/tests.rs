3809e052f0ad779d3fa77760e3eb651ad2bc5387
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-05-09 18:38:38 +0300

    #1320 add tests

--- src/solutions/minimum_distance_to_type_a_word_using_two_fingers/tests.rs
+++ src/solutions/minimum_distance_to_type_a_word_using_two_fingers/tests.rs
@@ -6,3 +6,24 @@ fn example1() {
     let output = 3;
     assert_eq!(output, Solution::minimum_distance(input));
 }
+
+#[test]
+fn example2() {
+    let input = "HAPPY".to_owned();
+    let output = 6;
+    assert_eq!(output, Solution::minimum_distance(input));
+}
+
+#[test]
+fn example3() {
+    let input = "NEW".to_owned();
+    let output = 3;
+    assert_eq!(output, Solution::minimum_distance(input));
+}
+
+#[test]
+fn example4() {
+    let input = "YEAR".to_owned();
+    let output = 7;
+    assert_eq!(output, Solution::minimum_distance(input));
+}
