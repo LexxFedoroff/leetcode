18dc2a53e1a9e479ea49fc4293a12e9242f75698
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-05-23 01:23:03 +0300

    #410 first solution

--- src/solutions/split_array_largest_sum/mod.rs
+++ src/solutions/split_array_largest_sum/mod.rs
@@ -4,7 +4,30 @@ use crate::Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn split_array(nums: Vec<i32>, m: i32) -> i32 {
-        0
+        Self::split_rec(&nums, m)
+    }
+
+    fn split_rec(tail: &[i32], m: i32) -> i32 {
+        if m == 1 {
+            return tail.iter().sum();
+        }
+
+        let n = tail.len();
+        let to = 1 + n - m as usize;
+
+        let mut min = None;
+
+        for i in 1..to as usize {
+            let sum1 = tail[0..i].iter().sum();
+            let sum2 = Self::split_rec(&tail[i..n], m - 1);
+            let max_sum = std::cmp::max(sum1, sum2);
+            min = match min {
+                Some(m) => Some(std::cmp::min(m, max_sum)),
+                None => Some(max_sum),
+            };
+        }
+
+        min.unwrap()
     }
 }
 
