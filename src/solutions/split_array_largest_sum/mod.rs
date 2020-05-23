adf7c5b4b38c9bc59bb4d45810b28a5016bb9d4f
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-05-23 14:56:45 +0300

    #410 optimize

--- src/solutions/split_array_largest_sum/mod.rs
+++ src/solutions/split_array_largest_sum/mod.rs
@@ -1,25 +1,31 @@
 // https://leetcode.com/problems/split-array-largest-sum/
 use crate::Solution;
+use std::collections::HashMap;
 
 impl Solution {
     #[allow(dead_code)]
     pub fn split_array(nums: Vec<i32>, m: i32) -> i32 {
-        Self::split_rec(&nums, m)
+        Self::split_rec(&nums, m, &mut HashMap::new())
     }
 
-    fn split_rec(tail: &[i32], m: i32) -> i32 {
+    fn split_rec(tail: &[i32], m: i32, memo: &mut HashMap<(usize, i32), i32>) -> i32 {
+        let n = tail.len();
+
+        if let Some(min) = memo.get(&(n, m)) {
+            return *min;
+        }
+
         if m == 1 {
             return tail.iter().sum();
         }
 
-        let n = tail.len();
         let to = 1 + n - m as usize;
 
         let mut min = None;
 
         for i in 1..to + 1 as usize {
             let sum1 = tail[0..i].iter().sum();
-            let sum2 = Self::split_rec(&tail[i..n], m - 1);
+            let sum2 = Self::split_rec(&tail[i..n], m - 1, memo);
             let max_sum = std::cmp::max(sum1, sum2);
             min = match min {
                 Some(m) => Some(std::cmp::min(m, max_sum)),
@@ -27,7 +33,9 @@ impl Solution {
             };
         }
 
-        min.unwrap()
+        let min = min.unwrap();
+        memo.entry((n, m)).or_insert(min);
+        min
     }
 }
 
