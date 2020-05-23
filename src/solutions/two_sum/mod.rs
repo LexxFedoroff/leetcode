137f6181a50af045c45f05c7feb900b0881ea7c0
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-05-23 22:42:00 +0300

    #1 add solution

--- src/solutions/two_sum/mod.rs
+++ src/solutions/two_sum/mod.rs
@@ -1,10 +1,23 @@
-//
+// https://leetcode.com/problems/two-sum/
 use crate::Solution;
+use std::collections::HashMap;
 
 impl Solution {
     #[allow(dead_code)]
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        nums
+        let mut nums_map = HashMap::<i32, usize>::new();
+        for (idx, num) in nums.iter().enumerate() {
+            nums_map.insert(*num, idx);
+        }
+
+        for (idx, num) in nums.iter().enumerate() {
+            let rest = target - num;
+            if let Some(idx2) = nums_map.get(&rest) {
+                return vec![idx as i32, *idx2 as i32];
+            }
+        }
+
+        Vec::new()
     }
 }
 
