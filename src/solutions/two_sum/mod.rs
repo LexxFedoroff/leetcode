56eec76e4648d82ecbc4acd1b9f80da635afd3ee
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-05-24 12:22:58 +0300

    #1 fix

--- src/solutions/two_sum/mod.rs
+++ src/solutions/two_sum/mod.rs
@@ -5,10 +5,7 @@ use std::collections::HashMap;
 impl Solution {
     #[allow(dead_code)]
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut nums_map = HashMap::<i32, usize>::new();
-        for (idx, num) in nums.iter().enumerate() {
-            nums_map.insert(*num, idx);
-        }
+        let nums_map: HashMap<i32, usize> = nums.iter().enumerate().map(|(i, v)| (*v, i)).collect();
 
         for (idx, num) in nums.iter().enumerate() {
             let rest = target - num;
