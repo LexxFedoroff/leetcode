d2e9f50cca48b9ee405062ac119f95eb99ed42dc
Alexey Fedorov <lexxfedoroff@gmail.com>
2020-08-22 16:52:39 +0300

    #15 refactor

--- src/solutions/three_sum/mod.rs
+++ src/solutions/three_sum/mod.rs
@@ -2,28 +2,51 @@
 use crate::Solution;
 use std::collections::HashSet;
 
-fn is_zero(vec: &Vec<i32>) -> bool {
-    vec.iter().fold(0, |acc, x| acc + x) == 0
+struct Result {
+    array: Vec<Vec<i32>>,
+    unique: HashSet<Vec<i32>>,
+}
+
+impl Result {
+    fn new() -> Self {
+        Result {
+            array: Vec::default(),
+            unique: HashSet::default(),
+        }
+    }
+
+    fn append(&mut self, a: i32, b: i32, c: i32) {
+        let mut vec = vec![a, b, c];
+        if !Self::is_zero(&vec) {
+            return;
+        }
+        vec.sort();
+        if !self.unique.contains(&vec) {
+            self.unique.insert(vec.clone());
+            self.array.push(vec);
+        }
+    }
+
+    fn is_zero(vec: &Vec<i32>) -> bool {
+        vec.iter().fold(0, |acc, x| acc + x) == 0
+    }
 }
 
 impl Solution {
     #[allow(dead_code)]
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut res = Vec::default();
-        let mut hs: HashSet<Vec<i32>> = HashSet::default();
+        let mut res = Result::new();
         for i in 0..nums.len() {
+            let i_val = nums[i];
             for j in i + 1..nums.len() {
+                let j_val = nums[j];
                 for k in j + 1..nums.len() {
-                    let mut vec = vec![nums[i], nums[j], nums[k]];
-                    vec.sort();
-                    if is_zero(&vec) && !hs.contains(&vec) {
-                        hs.insert(vec.clone());
-                        res.push(vec);
-                    }
+                    let k_val = nums[k];
+                    res.append(i_val, j_val, k_val);
                 }
             }
         }
-        return res;
+        return res.array;
     }
 }
 
--- src/solutions/three_sum/tests.rs
+++ src/solutions/three_sum/tests.rs
@@ -2,7 +2,14 @@ use crate::Solution;
 
 #[test]
 fn example1() {
+    let empty: Vec<Vec<i32>> = Vec::default();
+    assert_eq!(empty, Solution::three_sum(Vec::default()));
+
     let input = vec![-1, 0, 1, 2, -1, -4];
     let output = vec![vec![-1, 0, 1], vec![-1, -1, 2]];
+    assert_eq!(output, Solution::three_sum(input));
+
+    let input = vec![3, 0, -2, -1, 1, 2];
+    let output = vec![vec![-2, -1, 3], vec![-2, 0, 2], vec![-1, 0, 1]];
     assert_eq!(output, Solution::three_sum(input));
 }
